dd2cf1e657b260f6b16cd916faa74da105de3155
David <davidngacha01@gmail.com>
2023-08-18 00:28:06 +0300

    fix:add tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchCharacters } from '../utils/api';
 
 const HomePage: React.FC = () => {
+    const [characters, setCharacters] =useState < Character[]>([]);
+
+    useEffect (() => {
+        fetchCharacters().then((data) => {
+            setCharacters(data);
+        });
+    }, []);
     return (
         <div>
             <h1>Welcome to the Harry Potter App</h1>
-            {}
+            {/* Render the list of characters */}
         </div>
     );
 };
